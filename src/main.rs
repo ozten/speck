7c6db3292eba8b3fd0b8859fcb6cd23230026a9c
Austin King <shout@ozten.com>
2026-02-20 19:09:10 +0000

    speck-wgu: Add SPECK_RECORD env var support with recording context

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@
 use std::process::ExitCode;
 
 fn main() -> ExitCode {
+    // If SPECK_RECORD is set, create a recording context that writes a
+    // cassette file on drop. This is an internal developer mechanism and
+    // is not exposed in --help.
+    let _recording_ctx = std::env::var("SPECK_RECORD")
+        .ok()
+        .map(|path| speck::context::ServiceContext::recording(std::path::Path::new(&path)));
+
     match speck::run(std::env::args()) {
         Ok(()) => ExitCode::SUCCESS,
         Err(err) => {
